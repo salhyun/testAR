0dcb8aea5eb3751e7e2776e5dcecae8c03cdb8a8
salhyun <salhyun@naver.com>
2017-10-12 19:22:48 +0900

    Initial commit

---
+++ Assets/Scripts/myManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myManager : MonoBehaviour {
+
+	public void change_tex()
+	{
+		GameObject capsule = GameObject.Find ("Capsule");
+		Debug.Log ("pressed button. object name = " + capsule.name);
+
+		//capsule.GetComponent<Renderer> ().material.color = Color.green;
+
+		Material material = capsule.GetComponent<Renderer> ().material;
+
+		//material.color = Color.blue;
+		material.SetTexture ("_MainTex", Resources.Load ("images/150463757969262") as Texture);
+	}
+}
---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class sceneManager : MonoBehaviour {
+
+	public GameObject capsule;
+
+	private CameraDevice camDevice;
+	private Color capsuleColor;
+	private Image camImage;
+
+	// Use this for initialization
+	void Start () {
+		camDevice = CameraDevice.Instance;
+		bool enable = camDevice.SetFrameFormat (Image.PIXEL_FORMAT.RGB888, true);
+		Debug.Log ("camDevice setFormat = " + enable);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		bool isActive = camDevice.IsActive ();
+		Debug.Log ("camDevice active = " + isActive);
+
+		camImage = camDevice.GetCameraImage (Image.PIXEL_FORMAT.RGB888);
+		if (camImage != null) {
+			Debug.Log ("camera width = " + camImage.Width + ", height = " + camImage.Height);
+
+			Texture2D texImage = new Texture2D (camImage.Width, camImage.Height);
+			camImage.CopyToTexture (texImage);
+
+			//texImage = Resources.Load ("images/150463757969262") as Texture2D ;
+
+			Material material = capsule.GetComponent<Renderer> ().material;
+			material.SetTexture ("_MainTex", texImage as Texture);
+		}
+		else
+			Debug.Log ("camImage : null");
+
+		/*
+		Texture2D texImage = new Texture2D (camImage.Width, camImage.Height);
+		camImage.CopyToTexture (texImage);
+
+		Material material = capsule.GetComponent<Renderer> ().material;
+		material.SetTexture ("_MainTex", texImage);
+		*/
+	}
+}
